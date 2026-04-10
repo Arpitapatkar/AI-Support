84c2b10c65de4d259c3460d25a2c3018230b7808
Arpita <Arpita@gmail.com>
2026-04-10 22:51:28 +0530

    remove data file

--- src/constants.js
+++
@@ -1,4 +0,0 @@
-export const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=AIzaSyDrtCLvHiz2FjNLxRisuMmgZ-EuU9LZ7xg";
-//export const URL = import.meta.env.VITE_API_KEY;
-  
-
